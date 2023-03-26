c5ee32ba2e7706fd68c37a6b17bc63be78da0293
Brusnicka <yerbamaniac@gmail.com>
2023-03-26 17:01:18 +0400

    Add additional files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import 'bulma/css/bulma.min.css'
+import NavBar from './components/NavBar'
+import GeneratedData from './components/GeneratedData'
+
+function App() {
+    return (
+        <div className='container'>
+            <NavBar/>
+            <GeneratedData/>
+        </div>
+    )}
+
+export default App
